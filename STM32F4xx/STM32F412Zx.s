b7f4970564289b5fd1502a0ba373d9276fa75762
Mihai <bdmihai@gmail.com>
2023-12-15 23:17:05 +0100

    fixed typo for 412zg

--- STM32F4xx/STM32F412Zx.s
+++ STM32F4xx/STM32F412Zx.s
@@ -375,7 +375,7 @@ vector_table:
 .weak QUADSPI_IRQHandler
 .thumb_set QUADSPI_IRQHandler, Default_Handler
 
-.weakFMPI2C1_EV_IRQHandler
+.weak FMPI2C1_EV_IRQHandler
 .thumb_set FMPI2C1_EV_IRQHandler, Default_Handler
 
 .weak FMPI2C1_ER_IRQHandler
